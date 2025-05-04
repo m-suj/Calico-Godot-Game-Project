1e273bc9c6b7342db2b5cf1a11fbaef53d444d7f
m-suj <mateusz.sujewicz1234567@gmail.com>
2025-05-05 01:24:49 +0200

    menu pauzy prawie ukończone
    do zaimplementowania menu opcji i wyjście do menu głównego

--- scripts/tutorial_level/pause_menu.gd
+++ scripts/tutorial_level/pause_menu.gd
@@ -10,3 +10,13 @@ func _toggle_pause():
 	var tree = get_tree()
 	tree.paused = not tree.paused
 	visible = tree.paused
+
+
+
+func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
