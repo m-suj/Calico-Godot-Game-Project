22a5445bc61e3966c783bd9c0cba9b582af1db86
m-suj <mateusz.sujewicz1234567@gmail.com>
2025-05-05 01:42:51 +0200

    poprawiłem błąd z pauzowaniem sceny po przejściu z  poziomu do menu głównego

--- scripts/tutorial_level/pause_menu.gd
+++ scripts/tutorial_level/pause_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+func _ready() -> void:
+	visible = false
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("menu"):
 		_toggle_pause()
@@ -25,4 +29,6 @@ func _on_quit_button_pressed() -> void:
 func _on_main_menu_button_pressed() -> void:
 	# TODO: dialog zapytania o wyjście do menu głównego
 	# TODO: zapisanie stanu gry
-	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+	var tree = get_tree()
+	tree.paused = false
+	tree.change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
