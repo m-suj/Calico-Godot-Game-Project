bfed6a9f22aa8389aaa02ab55f006b25d440b732
m-suj <mateusz.sujewicz1234567@gmail.com>
2025-06-01 23:55:24 +0200

    Updated new resolution menu
    finished

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -15,5 +15,5 @@ func _on_new_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tutorial_level/tutorial_level.tscn")
 
 
-func _on_power_button_pressed() -> void:
+func _on_exit_button_pressed() -> void:
 	get_tree().quit()
